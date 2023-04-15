9255e2d4886915986849f504ee88237c51334ef4
Victor Wallsten <94681407+victorwallsten@users.noreply.github.com>
2023-04-15 20:00:41 +0200

    Create barbarian skill enum

---
+++ lib/src/enums/barbarian_skill.dart
@@ -0,0 +1,174 @@
+// ignore_for_file: constant_identifier_names
+
+enum BarbarianSkill {
+  /* Basic */
+  Bash,
+  EnhancedBash,
+  BattleBash,
+  CombatBash,
+
+  Flay,
+  EnhancedFlay,
+  BattleFlay,
+  CombatFlay,
+
+  Frenzy,
+  EnhancedFrenzy,
+  BattleFrenzy,
+  CombatFrenzy,
+
+  LungingStrike,
+  EnhancedLungingStrike,
+  BattleLungingStrike,
+  CombatLungingStrike,
+
+  /* Core */
+  DoubleSwing,
+  EnhancedDoubleSwing,
+  FuriousDoubleSwing,
+  ViolentDoubleSwing,
+
+  HammerOfTheAncients,
+  EnhancedHammerOfTheAncients,
+  FuriousHammerOfTheAncients,
+  ViolentHammerOfTheAncients,
+
+  Rend,
+  EnhancedRend,
+  FuriousRend,
+  ViolentRend,
+
+  Upheaval,
+  EnhancedUpheaval,
+  FuriousUpheaval,
+  ViolentUpheaval,
+
+  Whirlwind,
+  EnhancedWhirlwind,
+  FuriousWhirlwind,
+  ViolentWhirlwind,
+
+  PressurePoint,
+
+  EndlessFury,
+
+  /* Defensive */
+  ChallengingShout,
+  EnhancedChallengingShout,
+  StrategicChallengingShout,
+  TacticalChallengingShout,
+
+  GroundStomp,
+  EnhancedGroundStomp,
+  StrategicGroundStomp,
+  TacticalGroundStomp,
+
+  IronSkin,
+  EnhancedIronSkin,
+  StrategicIronSkin,
+  TacticalIronSkin,
+
+  RallyingCry,
+  EnhancedRallyingCry,
+  StrategicRallyingCry,
+  TacticalRallyingCry,
+
+  ImposingPresence,
+  MartialVigor,
+
+  Outburst,
+  ToughAsNails,
+
+  /* Brawling */
+  Charge,
+  EnhancedCharge,
+  MightyCharge,
+  PowerCharge,
+
+  Kick,
+  EnhancedKick,
+  MightyKick,
+  PowerKick,
+
+  Leap,
+  EnhancedLeap,
+  MightyLeap,
+  PowerLeap,
+
+  WarCry,
+  EnhancedWarCry,
+  MightyWarCry,
+  PowerWarCry,
+
+  BoomingVoice,
+  RaidLeader,
+  GutturalYell,
+
+  AggressiveResistance,
+  BattleFervor,
+  ProlificFury,
+
+  Swiftness,
+  QuickImpulses,
+
+  /* Weapon Mastery */
+  DeathBlow,
+  EnhancedDeathBlow,
+  FightersDeathBlow,
+  WarriorsDeathBlow,
+
+  Rupture,
+  EnhancedRupture,
+  FightersRupture,
+  WarriorsRupture,
+
+  SteelGrasp,
+  EnhancedSteelGrasp,
+  FightersSteelGrasp,
+  WarriorsSteelGrasp,
+
+  PitFighter,
+  NoMercy,
+  SlayingStrike,
+  ExposeVulnerability,
+
+  ThickSkin,
+  DefensiveStance,
+  Counteroffensive,
+
+  Hamstring,
+  CutToTheBone,
+
+  /* Ultimate */
+  CallOfTheAncients,
+  PrimeCallOfTheAncients,
+  SupremeCallOfTheAncients,
+
+  IronMaelstrom,
+  PrimeIronMaelstrom,
+  SupremeIronMaelstrom,
+
+  WrathOfTheBerserker,
+  PrimeWrathOfTheBerserker,
+  SupremeWrathOfTheBerserker,
+
+  HeavyHanded,
+  Wallop,
+  BruteForce,
+  Concussion,
+
+  TemperedFury,
+  FuriousImpulse,
+  InvigoratingFury,
+
+  Duelist,
+
+  /* Key Passive */
+  Unconstrained,
+
+  WalkingArsenal,
+
+  UnbridledRage,
+
+  GushingWounds,
+}
