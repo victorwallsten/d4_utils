fcd8c9b3ada98927e6d39edbfc63e85083182240
Victor Wallsten <94681407+victorwallsten@users.noreply.github.com>
2023-04-15 20:22:04 +0200

    Create tree data structure

---
+++ lib/src/data_structures/tree.dart
@@ -0,0 +1,19 @@
+class Tree<E> {
+  Tree({
+    required this.element,
+    this.children = const [],
+  });
+
+  E element;
+  List<Tree<E>> children;
+
+  T fold<T>(T initialValue, T Function(T previousValue, E element) combine) =>
+      children.fold(combine(initialValue, this.element),
+          (acc, child) => combine(acc, child.element));
+
+  Tree<T> map<T>(T Function(E element) morph) => Tree(
+        element: morph(this.element),
+        children:
+            children.map((child) => child.map(morph)).toList(growable: false),
+      );
+}
