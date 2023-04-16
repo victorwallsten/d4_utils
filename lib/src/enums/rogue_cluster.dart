a8dfd8f981004c40ee878338c494cba60f5a5063
Victor Wallsten <94681407+victorwallsten@users.noreply.github.com>
2023-04-16 17:55:27 +0200

    Create rogue cluster, skill enums

---
+++ lib/src/enums/rogue_cluster.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: constant_identifier_names
+
+enum RogueCluster {
+  Basic,
+  Core,
+  Agility,
+  Subterfuge,
+  Imbuement,
+  Ultimate,
+  KeyPassive,
+}
---
+++ lib/src/enums/rogue_skill.dart
@@ -0,0 +1,176 @@
+// ignore_for_file: constant_identifier_names
+
+enum RogueSkill {
+  /* Basic */
+  BladeShift,
+  EnhancedBladeShift,
+  FundamentalBladeShift,
+  PrimaryBladeShift,
+
+  ForcefulArrow,
+  EnhancedForcefulArrow,
+  FundamentalForcefulArrow,
+  PrimaryForcefulArrow,
+
+  Heartseeker,
+  EnhancedHeartseeker,
+  FundamentalHeartseeker,
+  PrimaryHeartseeker,
+
+  InvigoratingStrike,
+  EnhancedInvigoratingStrike,
+  FundamentalInvigoratingStrike,
+  PrimaryInvigoratingStrike,
+
+  Puncture,
+  EnhancedPuncture,
+  FundamentalPuncture,
+  PrimaryPuncture,
+
+  /* Core */
+  Barrage,
+  EnhancedBarrage,
+  AdvancedBarrage,
+  ImprovedBarrage,
+
+  Flurry,
+  EnhancedFlurry,
+  AdvancedFlurry,
+  ImprovedFlurry,
+
+  PenetratingShot,
+  EnhancedPenetratingShot,
+  AdvancedPenetratingShot,
+  ImprovedPenetratingShot,
+
+  RapidFire,
+  EnhancedRapidFire,
+  AdvancedRapidFire,
+  ImprovedRapidFire,
+
+  TwistingBlades,
+  EnhancedTwistingBlades,
+  AdvancedTwistingBlades,
+  ImprovedTwistingBlades,
+
+  Sturdy,
+  SiphoningStrikes,
+
+  StutterStep,
+
+  /* Agility */
+  Caltrops,
+  EnhancedCaltrops,
+  DisciplinedCaltrops,
+  MethodicalCaltrops,
+
+  Dash,
+  EnhancedDash,
+  DisciplinedDash,
+  MethodicalDash,
+
+  ShadowStep,
+  EnhancedShadowStep,
+  DisciplinedShadowStep,
+  MethodicalShadowStep,
+
+  Concussive,
+  RapidGambits,
+  TrickAttacks,
+
+  Rugged,
+  ReactiveDefense,
+
+  WeaponMastery,
+
+  /* Subterfuge */
+  Concealment,
+  EnhancedConcealment,
+  CounteringConcealment,
+  SubvertingConcealment,
+
+  DarkShroud,
+  EnhancedDarkShroud,
+  CounteringDarkShroud,
+  SubvertingDarkShroud,
+
+  PoisonTrap,
+  EnhancedPoisonTrap,
+  CounteringPoisonTrap,
+  SubvertingPoisonTrap,
+
+  SmokeGrenade,
+  EnhancedSmokeGrenade,
+  CounteringSmokeGrenade,
+  SubvertingSmokeGrenade,
+
+  Exploit,
+  Malice,
+
+  Agile,
+  MendingObscurity,
+
+  /* Imbuement */
+  ColdImbuement,
+  EnhancedColdImbuement,
+  BlendedColdImbuement,
+  MixedColdImbuement,
+
+  PoisonImbuement,
+  EnhancedPoisonImbuement,
+  BlendedPoisonImbuement,
+  MixedPoisonImbuement,
+
+  ShadowImbuement,
+  EnhancedShadowImbuement,
+  BlendedShadowImbuement,
+  MixedShadowImbuement,
+
+  DeadlyVenom,
+  DebilitatingToxins,
+  AlchemicalAdvantage,
+
+  ShadowCrash,
+  ConsumingShadows,
+
+  FrigidFinesse,
+  ChillingWeight,
+
+  PrecisionImbuement,
+
+  /* Ultimate */
+  DeathTrap,
+  PrimeDeathTrap,
+  SupremeDeathTrap,
+
+  RainOfArrows,
+  PrimeRainOfArrows,
+  SupremeRainOfArrows,
+
+  ShadowClone,
+  PrimeShadowClone,
+  SupremeShadowClone,
+
+  Innervation,
+  AlchemistsFortune,
+  SecondWind,
+
+  AdrenalineRush,
+  Haste,
+  Impetus,
+
+  TrapMastery,
+
+  Aftermath,
+
+  /* Key Passive */
+  Momentum,
+
+  CloseQuartersCombat,
+
+  Precision,
+
+  Victimize,
+
+  Exposure,
+}
