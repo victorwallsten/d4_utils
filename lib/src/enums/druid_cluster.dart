c278c447bd5a28ba13043419c8f40a30a1895966
Victor Wallsten <94681407+victorwallsten@users.noreply.github.com>
2023-04-16 13:08:30 +0200

    Create druid cluster, skill enums

---
+++ lib/src/enums/druid_cluster.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: constant_identifier_names
+
+enum DruidCluster {
+  Basic,
+  Core,
+  Defensive,
+  Companion,
+  Wrath,
+  Ultimate,
+  KeyPassive,
+}
---
+++ lib/src/enums/druid_skill.dart
@@ -0,0 +1,191 @@
+// ignore_for_file: constant_identifier_names
+
+enum DruidSkill {
+  /* Basic */
+  Maul,
+  EnhancedMaul,
+  FierceMaul,
+  WildMaul,
+
+  Claw,
+  EnhancedClaw,
+  FierceClaw,
+  WildClaw,
+
+  StormStrike,
+  EnhancedStormStrike,
+  FierceStormStrike,
+  WildStormStrike,
+
+  WindShear,
+  EnhancedWindShear,
+  FierceWindShear,
+  WildWindShear,
+
+  EarthSpike,
+  EnhancedEarthSpike,
+  FierceEarthSpike,
+  WildEarthSpike,
+
+  /* Core */
+  LightningStorm,
+  EnhancedLightningStorm,
+  PrimalLightningStorm,
+  RagingLightningStorm,
+
+  Tornado,
+  EnhancedTornado,
+  PrimalTornado,
+  RagingTornado,
+
+  Pulverize,
+  EnhancedPulverize,
+  PrimalPulverize,
+  RagingPulverize,
+
+  Shred,
+  EnhancedShred,
+  PrimalShred,
+  RagingShred,
+
+  LandSlide,
+  EnhancedLandSlide,
+  PrimalLandSlide,
+  RagingLandSlide,
+
+  HeartOfTheWild,
+  WildImpulses,
+  Abundance,
+
+  PredatoryInstinct,
+  DigitigradeGait,
+  IronFur,
+
+  /* Defensive */
+  EarthenBulwark,
+  EnhancedEarthenBulwark,
+  InnateEarthenBulwark,
+  PreservingEarthenBulwark,
+
+  CycloneArmor,
+  EnhancedCycloneArmor,
+  InnateCycloneArmor,
+  PreservingCycloneArmor,
+
+  BloodHowl,
+  EnhancedBloodHowl,
+  InnateBloodHowl,
+  PreservingBloodHowl,
+
+  DebilitatingRoar,
+  EnhancedDebilitatingRoar,
+  InnateDebilitatingRoar,
+  PreservingDebilitatingRoar,
+
+  AncestralFortitude,
+  Vigilance,
+
+  /* Companion */
+  Wolves,
+  EnhancedWolves,
+  BrutalWolves,
+  FerociousWolves,
+
+  Ravens,
+  EnhancedRavens,
+  BrutalRavens,
+  FerociousRavens,
+
+  VineCreeper,
+  EnhancedVineCreeper,
+  BrutalVineCreeper,
+  FerociousVineCreeper,
+
+  NaturesReach,
+
+  CallOfTheWild,
+
+  Clarity,
+
+  /* Wrath */
+  Boulder,
+  EnhancedBoulder,
+  NaturalBoulder,
+  SavageBoulder,
+
+  Trample,
+  EnhancedTrample,
+  NaturalTrample,
+  SavageTrample,
+
+  Hurricane,
+  EnhancedHurricane,
+  NaturalHurricane,
+  SavageHurricane,
+
+  Rabies,
+  EnhancedRabies,
+  NaturalRabies,
+  SavageRabies,
+
+  CrushingEarth,
+  Safeguard,
+  StoneGuard,
+
+  Neurotoxin,
+  ToxicClaws,
+  Envenom,
+
+  Mending,
+  Provocation,
+
+  ElementalExposure,
+  ChargedAtmosphere,
+  EndlessTempest,
+  BadOmen,
+  ElectricShock,
+
+  /* Ultimate */
+  GrizzlyRage,
+  PrimeGrizzlyRage,
+  SupremeGrizzlyRage,
+
+  Petrify,
+  PrimePetrify,
+  SupremePetrify,
+
+  Lacerate,
+  PrimeLacerate,
+  SupremeLacerate,
+
+  Cataclysm,
+  PrimeCataclysm,
+  SupremeCataclysm,
+
+  QuickShift,
+  NaturalFortitude,
+  HeightenedSenses,
+
+  DefensivePosture,
+  ThickHide,
+  NaturesResolve,
+  Unrestrained,
+
+  Defiance,
+  NaturalDisaster,
+  CircleOfLife,
+  Resonance,
+
+  /* Key Passive */
+  NaturesFury,
+
+  EarthenMight,
+
+  LupineFerocity,
+
+  BestialRampage,
+
+  PerfectStorm,
+
+  UrsineStrength,
+}
