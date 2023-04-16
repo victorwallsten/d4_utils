f059d056e2deeebbaafe66d6b87efaa6cd3a7ac9
Victor Wallsten <94681407+victorwallsten@users.noreply.github.com>
2023-04-16 18:21:56 +0200

    Create sorcerer cluster, skill enums

---
+++ lib/src/enums/sorcerer_cluster.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: constant_identifier_names
+
+enum SorcererCluster {
+  Basic,
+  Core,
+  Defensive,
+  Conjuration,
+  Mastery,
+  Ultimate,
+  KeyPassive,
+}
---
+++ lib/src/enums/sorcerer_skill.dart
@@ -0,0 +1,183 @@
+// ignore_for_file: constant_identifier_names
+
+enum SorcererSkill {
+  /* Basic */
+  Spark,
+  EnhancedSpark,
+  FlickeringSpark,
+  GlintingSpark,
+
+  FrostBolt,
+  EnhancedFrostBolt,
+  FlickeringFrostBolt,
+  GlintingFrostBolt,
+
+  FireBolt,
+  EnhancedFireBolt,
+  FlickeringFireBolt,
+  GlintingFireBolt,
+
+  ArcLash,
+  EnhancedArcLash,
+  FlickeringArcLash,
+  GlintingArcLash,
+
+  /* Core */
+  ChainLightning,
+  EnhancedChainLightning,
+  DestructiveChainLightning,
+  GreaterChainLightning,
+
+  ChargedBolts,
+  EnhancedChargedBolts,
+  DestructiveChargedBolts,
+  GreaterChargedBolts,
+
+  Fireball,
+  EnhancedFireball,
+  DestructiveFireball,
+  GreaterFireball,
+
+  FrozenOrb,
+  EnhancedFrozenOrb,
+  DestructiveFrozenOrb,
+  GreaterFrozenOrb,
+
+  Incinerate,
+  EnhancedIncinerate,
+  DestructiveIncinerate,
+  GreaterIncinerate,
+
+  IceShards,
+  EnhancedIceShards,
+  DestructiveIceShards,
+  GreaterIceShards,
+
+  Devastation,
+  ElementalDominance,
+
+  PotentWarding,
+
+  /* Defensive */
+  FlameShield,
+  EnhancedFlameShield,
+  MysticalFlameShield,
+  ShimmeringFlameShield,
+
+  IceArmor,
+  EnhancedIceArmor,
+  MysticalIceArmor,
+  ShimmeringIceArmor,
+
+  Teleport,
+  EnhancedTeleport,
+  MysticalTeleport,
+  ShimmeringTeleport,
+
+  FrostNova,
+  EnhancedFrostNova,
+  MysticalFrostNova,
+  ShimmeringFrostNova,
+
+  ElementalAttunement,
+
+  GlassCannon,
+
+  /* Conjuration */
+  Hydra,
+  EnhancedHydra,
+  InvokedHydra,
+  SummonedHydra,
+
+  IceBlades,
+  EnhancedIceBlades,
+  InvokedIceBlades,
+  SummonedIceBlades,
+
+  LightningSpear,
+  EnhancedLightningSpear,
+  InvokedLightningSpear,
+  SummonedLightningSpear,
+
+  AlignTheElements,
+  ManaShield,
+  Protection,
+
+  PrecisionMagic,
+
+  ConjurationMastery,
+
+  /* Mastery */
+  Blizzard,
+  EnhancedBlizzard,
+  MagesBlizzard,
+  WizardsBlizzard,
+
+  Firewall,
+  EnhancedFirewall,
+  MagesFirewall,
+  WizardsFirewall,
+
+  Meteor,
+  EnhancedMeteor,
+  MagesMeteor,
+  WizardsMeteor,
+
+  BallLightning,
+  EnhancedBallLightning,
+  MagesBallLightning,
+  WizardsBallLightning,
+
+  IcyVeil,
+  ColdFront,
+  SnapFreeze,
+
+  InnerFlames,
+  CripplingFlames,
+  DevouringBlaze,
+
+  StaticDischarge,
+  ShockingImpact,
+  InvigoratingConduit,
+
+  /* Ultimate */
+  DeepFreeze,
+  PrimeDeepFreeze,
+  SupremeDeepFreeze,
+
+  Inferno,
+  PrimeInferno,
+  SupremeInferno,
+
+  UnstableCurrents,
+  PrimeUnstableCurrents,
+  SupremeUnstableCurrents,
+
+  Permafrost,
+  Hoarfrost,
+  IcyTouch,
+  FrigidBreeze,
+
+  FierySurge,
+  EndlessPyre,
+  Soulfire,
+  Warmth,
+
+  CoursingCurrents,
+  Conduction,
+  Electrocution,
+  Convulsions,
+
+  /* Key Passive */
+  Avalanche,
+
+  Shatter,
+
+  OverflowingEnergy,
+
+  VyrsMastery,
+
+  EsusFerocity,
+
+  Combustion,
+}
