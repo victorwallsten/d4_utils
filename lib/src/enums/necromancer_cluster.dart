a5050573f7be0bb57396e9b923fba0c78caecdde
Victor Wallsten <94681407+victorwallsten@users.noreply.github.com>
2023-04-16 13:53:19 +0200

    Create necromancer cluster, skill enums

---
+++ lib/src/enums/necromancer_cluster.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: constant_identifier_names
+
+enum NecromancerCluster {
+  Basic,
+  Core,
+  FirstCorpseAndMacabre,
+  Curse,
+  SecondCorpseAndMacabre,
+  Ultimate,
+  KeyPassive,
+}
---
+++ lib/src/enums/necromancer_skill.dart
@@ -0,0 +1,158 @@
+// ignore_for_file: constant_identifier_names
+
+enum DruidSkill {
+  /* Basic */
+  BoneSplinters,
+  EnhancedBoneSplinters,
+  AcolytesBoneSplinters,
+  InitiatesBoneSplinters,
+
+  Decompose,
+  EnhancedDecompose,
+  AcolytesDecompose,
+  InitiatesDecompose,
+
+  Hemorrhage,
+  EnhancedHemorrhage,
+  AcolytesHemorrhage,
+  InitiatesHemorrhage,
+
+  Reap,
+  EnhancedReap,
+  AcolytesReap,
+  InitiatesReap,
+
+  /* Core */
+  Blight,
+  EnhancedBlight,
+  ParanormalBlight,
+  SupernaturalBlight,
+
+  BloodLance,
+  EnhancedBloodLance,
+  ParanormalBloodLance,
+  SupernaturalBloodLance,
+
+  BloodSurge,
+  EnhancedBloodSurge,
+  ParanormalBloodSurge,
+  SupernaturalBloodSurge,
+
+  BoneSpear,
+  EnhancedBoneSpear,
+  ParanormalBoneSpear,
+  SupernaturalBoneSpear,
+
+  Sever,
+  EnhancedSever,
+  ParanormalSever,
+  SupernaturalSever,
+
+  UnlivingEnergy,
+  ImperfectlyBalanced,
+
+  HewedFlesh,
+
+  /* First Corpse and Macabre */
+  BloodMist,
+  EnhancedBloodMist,
+  DreadfulBloodMist,
+  GhastlyBloodMist,
+
+  BonePrison,
+  EnhancedBonePrison,
+  DreadfulBonePrison,
+  GhastlyBonePrison,
+
+  CorpseExplosion,
+  EnhancedCorpseExplosion,
+  BlightedCorpseExplosion,
+  PlaguedCorpseExplosion,
+
+  GrimHarvest,
+  FueledByDeath,
+
+  SpikedArmor,
+
+  SkeletalWarriorMastery,
+
+  /* Curse */
+  Decrepify,
+  EnhancedDecrepify,
+  AbhorrentDecrepify,
+  HorridDecrepify,
+
+  IronMaiden,
+  EnhancedIronMaiden,
+  AbhorrentIronMaiden,
+  HorridIronMaiden,
+
+  DeathsEmbrace,
+
+  DeathsReach,
+
+  AmplifyDamage,
+
+  SkeletalMageMastery,
+
+  /* Second Corpse and Macabre */
+  BoneSpirit,
+  EnhancedBoneSpirit,
+  DreadfulBoneSpirit,
+  GhastlyBoneSpirit,
+
+  CorpseTendrils,
+  EnhancedCorpseTendrils,
+  BlightedCorpseTendrils,
+  PlaguedCorpseTendrils,
+
+  GruesomeMending,
+  Transfusion,
+  CoalescedBlood,
+  TidesOfBlood,
+  DrainVitality,
+
+  Serration,
+  CompoundFracture,
+  RapidOssification,
+  Evulsion,
+
+  ReapersPursuit,
+  Gloom,
+  CripplingDarkness,
+  Terror,
+
+  NecroticCarapace,
+
+  /* Ultimate */
+  ArmyOfTheDead,
+  PrimeArmyOfTheDead,
+  SupremeArmyOfTheDead,
+
+  BloodWave,
+  PrimeBloodWave,
+  SupremeBloodWave,
+
+  BoneStorm,
+  PrimeBoneStorm,
+  SupremeBoneStorm,
+
+  StandAlone,
+  MementoMori,
+
+  InspiringLeader,
+  BondedInEssence,
+  DeathsDefense,
+  HellbentCommander,
+
+  GolemMastery,
+
+  /* Key Passive */
+  OssifiedEssence,
+
+  RathmasVigor,
+
+  Shadowblight,
+
+  KalansEdict,
+}
