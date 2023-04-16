6a268e5d6064d5a33aa8314416e8e0dd46deb1d8
Victor Wallsten <94681407+victorwallsten@users.noreply.github.com>
2023-04-16 18:30:37 +0200

    Create druid, necro, rogue, sorc class bases

---
+++ lib/src/character_classes/druid.dart
@@ -0,0 +1,13 @@
+import 'package:d4_utils/src/data_structures/pair.dart';
+import 'package:d4_utils/src/data_structures/tree.dart';
+import 'package:d4_utils/src/enums/druid_cluster.dart';
+import 'package:d4_utils/src/enums/druid_skill.dart';
+import 'package:d4_utils/src/enums/character_class.dart';
+
+abstract class Druid {
+  static Tree<Pair<Enum, int>> _tree(
+          Enum e, List<Tree<Pair<Enum, int>>> children) =>
+      Tree(element: Pair(e, 0), children: children);
+
+  static List<Tree<Pair<Enum, int>>> get _childless => const [];
+}
