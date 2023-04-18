a91105243d8114a78257c93aa21cb0bf70a575ca
Victor Wallsten <94681407+victorwallsten@users.noreply.github.com>
2023-04-18 19:15:33 +0200

    Add cluster thresholds of remaining classes

--- lib/src/widgets/character_class_widget.dart
+++ lib/src/widgets/character_class_widget.dart
@@ -2,6 +2,10 @@ import 'package:d4_utils/src/data_structures/pair.dart';
 import 'package:d4_utils/src/data_structures/tree.dart';
 import 'package:d4_utils/src/enums/barbarian_cluster.dart';
 import 'package:d4_utils/src/enums/character_class.dart';
+import 'package:d4_utils/src/enums/druid_cluster.dart';
+import 'package:d4_utils/src/enums/necromancer_cluster.dart';
+import 'package:d4_utils/src/enums/rogue_cluster.dart';
+import 'package:d4_utils/src/enums/sorcerer_cluster.dart';
 import 'package:d4_utils/src/utils/enum_utils.dart';
 import 'package:d4_utils/src/widgets/cluster_widget.dart';
 import 'package:flutter/material.dart';
@@ -31,18 +35,46 @@ class _CharacterClassWidgetState extends State<CharacterClassWidget> {
   int _thresholdOf(Enum cluster) {
     switch (cluster) {
       case BarbarianCluster.Basic:
+      case DruidCluster.Basic:
+      case NecromancerCluster.Basic:
+      case RogueCluster.Basic:
+      case SorcererCluster.Basic:
         return 0;
       case BarbarianCluster.Core:
+      case DruidCluster.Core:
+      case NecromancerCluster.Core:
+      case RogueCluster.Core:
+      case SorcererCluster.Core:
         return 2;
       case BarbarianCluster.Defensive:
+      case DruidCluster.Defensive:
+      case NecromancerCluster.FirstCorpseAndMacabre:
+      case RogueCluster.Agility:
+      case SorcererCluster.Defensive:
         return 6;
       case BarbarianCluster.Brawling:
+      case DruidCluster.Companion:
+      case NecromancerCluster.Curse:
+      case RogueCluster.Subterfuge:
+      case SorcererCluster.Conjuration:
         return 11;
       case BarbarianCluster.WeaponMastery:
+      case DruidCluster.Wrath:
+      case NecromancerCluster.SecondCorpseAndMacabre:
+      case RogueCluster.Imbuement:
+      case SorcererCluster.Mastery:
         return 16;
       case BarbarianCluster.Ultimate:
+      case DruidCluster.Ultimate:
+      case NecromancerCluster.Ultimate:
+      case RogueCluster.Ultimate:
+      case SorcererCluster.Ultimate:
         return 23;
       case BarbarianCluster.KeyPassive:
+      case DruidCluster.KeyPassive:
+      case NecromancerCluster.KeyPassive:
+      case RogueCluster.KeyPassive:
+      case SorcererCluster.KeyPassive:
         return 33;
       default:
         assert(false, 'case for $cluster missing');
